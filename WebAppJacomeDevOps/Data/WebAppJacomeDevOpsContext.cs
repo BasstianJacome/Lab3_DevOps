2af2bef6803440bcab951b4ec823ee15b446038a
Vinicio Jacome <vjacomeg@my.centennialcollege.ca>
2024-03-15 23:40:39 -0400

    First commit

---
+++ WebAppJacomeDevOps/Data/WebAppJacomeDevOpsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppJacomeDevOps.Models;
+
+namespace WebAppJacomeDevOps.Data
+{
+    public class WebAppJacomeDevOpsContext : DbContext
+    {
+        public WebAppJacomeDevOpsContext (DbContextOptions<WebAppJacomeDevOpsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppJacomeDevOps.Models.Movie> Movie { get; set; } = default!;
+    }
+}
